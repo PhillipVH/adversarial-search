a64a4ad792c56b60395fa4f7b312776d1f1433b7
Zhunaid <zhunaid.m@gmail.com>
2018-09-16 21:27:40 +0200

    Implemented negascout

---
+++ src/main/java/za/ac/sun/cs/adversarial/algorithm/Negascout.java
@@ -0,0 +1,55 @@
+package za.ac.sun.cs.adversarial.algorithm;
+
+import za.ac.sun.cs.adversarial.domain.Board;
+import za.ac.sun.cs.adversarial.domain.Move;
+
+/**
+ * This class represents the NegaScout algorithm.
+ */
+public class Negascout {
+
+    /**
+     * The Negascout algorithm. (A Comparative Study of Game Tree Searching Methods, Fig 5)
+     * @return The value of the given node.
+     */
+
+    public static int NegaScout(Board board, int depth, int alpha, int beta, int player) {
+
+        if (depth == 0 || (board.isTerminal()) != -1)  {
+            return player;
+        }
+
+        int score = Integer.MIN_VALUE;
+        int n = beta;
+
+        // Generate successors
+        for (Move move: board.getLegalMoves()) {
+            // Execute current move.
+            board.makeMove(player, move);
+
+            // Call other player.
+            int current = -NegaScout(board, depth - 1, -n, -alpha, -player);
+
+            if (current > score) {
+                if (n == beta || depth <= 2) {
+                    score = current;
+                } else {
+                    score = -NegaScout(board, depth - 1, -beta, -current, -player);
+                }
+            }
+
+            // Adjust search window.
+            if (score > alpha) { alpha = score;}
+
+            // Undo move
+            board.undoMove(move);
+
+            // Cut offs.
+            if (alpha >= beta) { return alpha;}
+            n = alpha + 1;
+        }
+
+        return score;
+    }
+    
+}
---
+++ src/test/java/za/ac/sun/cs/search/adversarialagent/TestNegascout.java
@@ -0,0 +1,43 @@
+package za.ac.sun.cs.search.adversarialagent;
+
+import org.junit.Test;
+import za.ac.sun.cs.adversarial.agent.RandomAgent;
+import za.ac.sun.cs.adversarial.referee.Referee;
+import za.ac.sun.cs.adversarial.agent.Agent;
+import za.ac.sun.cs.adversarial.agent.NegamaxAgent;
+import za.ac.sun.cs.adversarial.agent.NegascoutAgent;
+
+
+public class TestNegascout {
+
+    @Test
+    public void smokeTest() {
+        Agent playerOne = new NegascoutAgent(3, 3, 3, 10, 1);
+        Agent playerTwo = new RandomAgent(3, 3, 3, 2);
+
+        Referee referee = new Referee(playerOne, playerTwo);
+
+        referee.runGame();
+    }
+
+    @Test
+    public void PruningPlayers() {
+        Agent playerOne = new NegascoutAgent(3, 3, 3, 10, 1);
+        Agent playerTwo = new NegamaxAgent(3, 3, 3, 10, 2);
+
+        Referee referee = new Referee(playerOne, playerTwo);
+
+        referee.runGame();
+    }
+
+    @Test
+    public void PruningPlayers2() {
+        Agent playerOne = new NegamaxAgent(3, 3, 3, 10, 1);
+        Agent playerTwo = new NegascoutAgent(3, 3, 3, 10, 2);
+
+        Referee referee = new Referee(playerOne, playerTwo);
+
+        referee.runGame();
+    }
+
+}
