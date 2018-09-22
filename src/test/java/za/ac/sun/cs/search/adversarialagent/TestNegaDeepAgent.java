a6dd87b9270dd52403c3f932c9364ff1d437006e
Phillip van Heerden <vanheerden.phillip@gmail.com>
2018-09-22 23:56:11 +0200

    Added a smoke test for the NegaDeep agent

---
+++ src/test/java/za/ac/sun/cs/search/adversarialagent/TestNegaDeepAgent.java
@@ -0,0 +1,20 @@
+package za.ac.sun.cs.search.adversarialagent;
+
+import org.junit.Test;
+import za.ac.sun.cs.adversarial.agent.Agent;
+import za.ac.sun.cs.adversarial.agent.NegaDeepAgent;
+import za.ac.sun.cs.adversarial.agent.RandomAgent;
+import za.ac.sun.cs.adversarial.referee.Referee;
+
+public class TestNegaDeepAgent {
+
+    @Test
+    public void smokeTest() {
+        Agent playerOne = new NegaDeepAgent(3, 3, 3, 10, 1);
+        Agent playerTwo = new RandomAgent(3, 3, 3, 2);
+
+        Referee referee = new Referee(playerOne, playerTwo);
+
+        referee.runGame();
+    }
+}
